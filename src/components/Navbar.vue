673b645ffbda3e160ac37e216f8fc5f572e6b9b6
Nina Matti <ninadesireematti@gmail.com>
2020-11-10 17:27:46 +0900

    Added basic component structure and logic
    Co-Authored-By: Jack Fowler <35797565+OnigiriJack@users.noreply.github.com>

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>Navbasfsadfsfsdfr</div>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+
+}
+</script>
+
+
+
+<style>
+
+
+
+</style>
---
+++ src/components/Total.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <div>
+        <h1>Total Progress Graph:</h1>
+        <div id="totalRow">
+            <graph />
+            <totalCompare />
+        </div>
+    </div>
+    
+</template>
+
+<script>
+import Graph from "./Graph.vue"
+import TotalCompare from "./TotalCompare"
+
+export default {
+  name: 'Total',
+  components: {
+      graph: Graph,
+      totalCompare: TotalCompare,
+  }
+
+}
+</script>
+
+<style>
+
+</style>
