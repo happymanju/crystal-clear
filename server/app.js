e48ada3087f50f5de308934afc7bec1c3ac1b0e3
Michael Piastucki <44167171+happymanju@users.noreply.github.com>
2020-11-10 17:52:27 +0900

    added knex and knex config

--- server/app.js
+++ server/app.js
@@ -1,6 +1,29 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
+const axios = require("axios");
+const db = require("./knex.js");
+
+async function fetchData() {
+    const url = 'https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/me';
+    const options = {
+        headers: {
+            Authorization: `Bearer ${process.env.API_KEY}`
+        }
+    }
+
+    await axios.get(url, options).then(data => console.log(data));
+}
+
+
+async function fetchDataPG() {
+    const users = await db.select().table("users");
+    console.log(users);
+}
+
+fetchDataPG();
+
 
 
 
