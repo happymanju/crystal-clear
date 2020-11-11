84292564e3069669a70fb1d9c6f6c9d39328e4c6
Michael Piastucki <44167171+happymanju@users.noreply.github.com>
2020-11-11 12:12:42 +0900

    added endpoint in API interface for radius, TODO: make variable inputs

--- server/app.js
+++ server/app.js
@@ -6,25 +6,45 @@ const axios = require("axios");
 const db = require("./knex.js");
 
 async function fetchData() {
-    const url = 'https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/me';
+    const url = 'https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/search/radius';
     const options = {
         headers: {
             Authorization: `Bearer ${process.env.API_KEY}`
-        }
+        },
+        params: { longitude: 139.7394763, latitude: 35.6728567, radius: 2000 }
     }
 
-    await axios.get(url, options).then(data => console.log(data));
+    try {
+        const taps = await axios.get(url, options).then(res => res.data.taps);
+        const tapsCategory = taps.map((tap) => tap.category_id);
+        return tapsCategory;
+    } catch (err) {
+        console.error(err);
+    }
+
 }
 
-
-async function fetchDataPG() {
-    const users = await db.select().table("users");
-    console.log(users);
-}
-
-fetchDataPG();
+const taps = fetchData();
 
 
+
+// async function fetchDataPG() {
+//     const users = await db.select().table("users");
+//     console.log(users);
+// }
+
+
+
+app.get("/express/radius", async (req, res) => {
+    const query = req.query;
+    const testCoordinates = { longitude: 139.73655447363853, latitude: 35.671812626599866, radius: 500 }
+    const fetchURL = "https://my-mizu-dev2-gen8n.ondigitalocean.app/dev-api/search/radius";
+
+    const result = await fetchData();
+    console.log(result)
+
+    res.send(result);
+})
 
 
 app.use(express.static(path.join(__dirname, "..", "dist")));
