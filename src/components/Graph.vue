fde68ed04cb7bab9ea8b1fe0de6cc9796a9409a4
Nina Matti <ninadesireematti@gmail.com>
2020-11-11 14:33:50 +0900

    made changes to graph component

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,21 +1,23 @@
 <template>
-
-    <div>
-        Here goes some awesome Graph!!!  
-    </div>
-    
+    <div  class="line_chart">
+        Chellou Graph!
+        <lineChart />
+    </div>    
 </template>
 
 <script>
-
+import LineChart from "./LineChart.vue";
 
 export default {
   name: 'Graph',
-  
-
+  components:{
+      LineChart
+  }
 }
 </script>
 
 <style>
-
+.line_chart {
+    background-color:aqua;
+}
 </style>
