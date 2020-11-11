788b8877aa433dc526499d37c306f967f00d4600
Nina Matti <ninadesireematti@gmail.com>
2020-11-11 14:30:45 +0900

    added linechart component for displaying total progress data

---
+++ src/components/LineChart.vue
@@ -0,0 +1,24 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  name: "LineChart",
+  extends: Line,
+  data: () => ({}),
+  mounted () {
+    this.renderChart({
+      labels: ['December','January', 'February', 'March', 'April', 'May'],
+      datasets: [
+        {
+          label: "Data One",
+          backgroundColor: "Blue",
+          data: [40, 20, 60, 10, 50, 45]
+        }
+      ]
+    }, {
+      responsive: true,
+      maintainAspectRatio: false
+    })
+  }
+}
+</script>
