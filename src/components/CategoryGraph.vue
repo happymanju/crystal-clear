a27930fa4e391318c044c25d782d50f8507ca3d5
Jack Fowler <OnigiriJack@github.com>
2020-11-11 16:23:08 +0900

    radio button for 3 shibuya locations

--- src/components/CategoryGraph.vue
+++ src/components/CategoryGraph.vue
@@ -10,7 +10,7 @@ export default {
     this.renderChart({
       labels: [
         "Not classified",
-        "Natural Springs",
+        "Spring Water",
         "Public",
         "Open Maps",
         "Private",
--- src/components/Total.vue
+++ src/components/Total.vue
@@ -4,7 +4,19 @@
     <div class="total-wrapper">
       <graph class="total-item" />
       <totalCompare class="total-item" />
-      <categoryGraph v-bind:categories="categories" class="total-item" />
+      <div class="total-item">
+        <div v-for="location in locations" :key="location.name">
+          <input
+            v-on:change="setLocation"
+            type="radio"
+            :value="location.coords"
+            v-model="picked"
+          />
+          <label for="location.name">{{ location.name }} </label>
+        </div>
+        <span>picked: {{ picked }} </span>
+        <categoryGraph v-bind:categories="categories" />
+      </div>
     </div>
   </div>
 </template>
@@ -23,16 +35,21 @@ export default {
   },
   data: () => ({
     categories: [],
+    picked: "picked",
+    locations: [
+      { name: "shibuya", coords: [139.697063, 35.667427] },
+      { name: "minato", coords: [139.728501, 35.6111] },
+      { name: "shimokitazawa", coords: [139.669767, 35.666203] },
+    ],
   }),
 
   mounted() {
     console.log("mounted");
-    this.getCatgories();
+    this.getCategories();
   },
   methods: {
-    async getCatgories() {
+    async getCategories() {
       await axios.get("/express/radius").then((data) => {
-        console.log(data.data);
         let arrayOfIds = data.data;
         let countedCategories = arrayOfIds.reduce((tally, id) => {
           if (!tally[id]) {
@@ -46,6 +63,9 @@ export default {
         console.log(this.categories);
       });
     },
+    setLocation() {
+      console.log(this.picked);
+    },
   },
 };
 </script>
@@ -58,9 +78,11 @@ export default {
   display: grid;
   background-color: yellow;
   grid-template-columns: repeat(auto-fill, minmax(300px, 4fr));
+  align-items: center;
   grid-gap: 20px;
 }
 .total-item {
+  flex-direction: column;
   display: flex;
   border: 1px solid red;
   border-radius: 5px;
