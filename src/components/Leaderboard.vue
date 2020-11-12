bff1285aea031306863353a86af3422cc8534898
Yoshi106 <yshkbr@gmail.com>
2020-11-12 12:27:50 +0900

    Added simple leaderboard compnent

--- src/components/Leaderboard.vue
+++ src/components/Leaderboard.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    LeaderBoard
+    <h>LeaderBoard</h>
+    <ol>
+      <li v-for="item in rankArr" :key="item[0]">
+        {{ item[0] }} {{ item[1] }} litters
+      </li>
+    </ol>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "LeaderBoard",
+  data: () => ({
+    rankArr: [],
+  }),
+  mounted: async function () {
+    const res = await axios.get("/express/leaderboard");
+    console.log(res.data);
+    this.rankArr = res.data;
+  },
 };
 </script>
 
