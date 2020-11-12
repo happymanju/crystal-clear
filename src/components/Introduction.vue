6474dbf7f002a1cf7734d0dd039d044d2dcfd301
Nina Matti <ninadesireematti@gmail.com>
2020-11-12 18:42:35 +0900

    added footer and other changes to general styling

---
+++ src/components/Introduction.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 id="logo-name">Chrystal Clear</h1>
+    <div class="introduction-box">
+        <div>
+        Our project intends to visualize the impact each person can have by switching from using single-use plastic bottles to refillable water bottles.
+        </div>
+    </div>  
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Introduction',
+}
+</script>
+
+<style>
+
+</style>
