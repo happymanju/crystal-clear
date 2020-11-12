2a2bad66b21bc0762a4096004513296a4ac722e6
Jack Fowler <OnigiriJack@github.com>
2020-11-12 15:52:02 +0900

    dynamic graphs with search

--- src/components/Total.vue
+++ src/components/Total.vue
@@ -10,7 +10,7 @@
           placeholder="enter lcoation"
           v-on:change="setUserLoc"
         />
-        <button v-on:click="getLocationcoord">chaneLocation</button>
+        <button v-on:click="getLocationcoord">SEARCH</button>
         <div
           v-on:change="setLocation"
           v-for="location in locations"
@@ -19,7 +19,7 @@
           <input type="radio" :value="location" v-model="picked" />
           <label for="location.name">{{ location.name }} </label>
         </div>
-        <span>picked: {{ picked }} </span>
+        <span>picked: {{ pickedName }} </span>
         <categoryGraph :chart-data="datacollection" />
       </div>
     </div>
@@ -42,9 +42,11 @@ export default {
   },
   data: () => ({
     categories: { "0": 0, "1": 0, "2": 9, "3": 0, "4": 0, "5": 0 },
-    picked: "picked",
+    picked: "Shibuya",
     userLoc: "Shibuya",
     pickedName: "Shibuya",
+    searchName: "shibuya",
+    searchLoc: [],
     locations: [
       { name: "Shibuya", coords: [35.677427, 139.697063] },
       { name: "Minato", coords: [35.6111, 139.728501] },
@@ -57,21 +59,73 @@ export default {
     this.getCategories();
   },
   methods: {
-    getLocationcoord() {
-      axios
-        .get("/googleLoc", {
-          params: {
-            query: this.userLoc,
-            // key: "AIzaSyD3wNIEAB9W2H07k3etD-prDjEV3IWmMqE",
+    async getLocationcoord() {
+      let apiData = await axios.get("/googleLoc", {
+        params: {
+          query: this.userLoc,
+        },
+      });
+      this.pickedName = apiData.data.results[0].name;
+      this.searchLoc = [
+        apiData.data.results[0].lat,
+        apiData.data.results[0].lon,
+      ];
+      let result = await axios.get("/express/radius", {
+        params: {
+          latitude: this.searchLoc[0],
+          longitude: this.searchLoc[1],
+          radius: 2000,
+        },
+      });
+
+      let arrayOfIds = result.data;
+      let countedCategories = arrayOfIds.reduce((tally, id) => {
+        if (!tally[id]) {
+          tally[id] = 1;
+        } else {
+          tally[id] = tally[id] + 1;
+        }
+        return tally;
+      }, {});
+      this.categories = countedCategories;
+      this.datacollection = {
+        labels: [
+          "Not classified",
+          "Spring Water",
+          "Public",
+          "Open Maps",
+          "Private",
+          "verification-pending",
+        ],
+        datasets: [
+          {
+            label: this.pickedName,
+            backgroundColor: [
+              "rgb(255, 148, 120)",
+              "rgba(0, 181, 204, 1)",
+              "rgba(65, 131, 215, 1)",
+              "rgba(83, 51, 237, 1)",
+              "rgba(252, 185, 65, 1)",
+              "rgba(145, 61, 136, 1)",
+            ],
+            borderColor: "rgba(58, 83, 155, 1)",
+            borderWidth: 1,
+            data: [
+              this.categories["0"],
+              this.categories["1"],
+              this.categories["2"],
+              this.categories["3"],
+              this.categories["4"],
+              this.categories["5"],
+            ],
           },
-        })
-        .then((data) => {
-          console.log("google results", data);
-        });
+        ],
+      };
     },
     setUserLoc() {
-      console.log(this.userLoc);
+      this.picked = this.searchLoc;
     },
+
     async getCategories() {
       let result = await axios.get("/express/radius", {
         params: {
@@ -126,8 +180,6 @@ export default {
     },
 
     async setLocation() {
-      console.log("this.picked", this.picked);
-
       let result = await axios.get("/express/radius", {
         params: {
           latitude: this.picked.coords[0],
@@ -135,7 +187,6 @@ export default {
           radius: 2000,
         },
       });
-      console.log(result.data);
       let arrayOfIds = result.data;
       let countedCategories = arrayOfIds.reduce((tally, id) => {
         if (!tally[id]) {
