225e2f6bfc69f6cd09ffad4e8f5e6e241dae224d
Michael Piastucki <44167171+happymanju@users.noreply.github.com>
2020-11-13 14:00:06 +0900

    changed app name to Crystal Clear

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="introduction-box">
-    <h1 id="logo-name">Chrystal Clear</h1>
+    <h1 id="logo-name">Crystal Clear</h1>
     <div>
       <h3 id="intro-text">
         We invite you to explore mymizu with real-time data.
