be7e5d8cdd30f423bc0c9a4cd611cbd913e015b4
Michael Piastucki <44167171+happymanju@users.noreply.github.com>
2020-11-13 12:04:30 +0900

    new into text

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -3,9 +3,8 @@
     <h1 id="logo-name">Chrystal Clear</h1>
     <div>
       <h3 id="intro-text">
-        Our project intends to visualize the impact each person can have by
-        switching from using single-use plastic bottles to refillable water
-        bottles.
+        See the real-time impact of mymizu with the data below.
+We hope this app helps to inspire and showcase our community!
       </h3>
     </div>
   </div>
